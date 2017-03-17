6bc377cea6f6d6994f6b642562c7444d7b27914d
Ivandro Ismael <ivandrofly@gmail.com>
2017-03-17 02:33:19 +0000

    add get function state.

--- basic_trig_func.py
+++ basic_trig_func.py
@@ -1,7 +1,16 @@
 import math
 
 # Get the period of sinusoidal function.
-def get_sinusoidal_period(b):
+def get_sinusoidal_period(B):
     x = (2 * math.pi) 
-    b = abs(b)
-    return x / b
+    B = abs(b)
+    return x / B
+
+# Given an A return if function is stretched or compressed.
+def get_function_state(A):
+    # TODO: ensure A is a number.
+    A = abs(A)
+    if abs(A) > 1:
+        print('Function is stretched')
+    else:
+        print('Function is compressed')
